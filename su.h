54eb06d0e6e9938a68122b519883157e471589ee
stefan11111 <stefanalexe48@gmail.com>
2023-02-09 15:51:38 +0200

    make global variables static

--- su.h
+++ su.h
@@ -9,6 +9,6 @@
 
 #define PWD_MAX 200
 #define MAX_PATH 200
-char HOME[MAX_PATH] = "HOME=/home/";
+static char HOME[MAX_PATH] = "HOME=/home/";
 
 #define SHELL "/bin/bash"	/* chose what shell to spawn if user han no shell */
